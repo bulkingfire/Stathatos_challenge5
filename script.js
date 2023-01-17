82d2e749f96c4efe50b8ccc2d1e1a0b47d3dbdcb
bulkingfire <evagelos18@gmail.com>
2023-01-16 20:57:53 -0600

    initial changes

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+var today = dayjs();
+console.log(today);
+$('#currentDay').text(today.format('MMM D, YYYY'));
+var curhour = today.$H;
+
+
+// TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+if (id > curhour){
+  var div = document.querySelector('div');
+  div.classList.replace('present', 'future');
+  div.classList.replace('past', 'future');
+}
+
+if (id < curhour){
+  var div = document.querySelector('div');
+  div.classList.replace('present', 'past');
+  div.classList.replace('future', 'past');
+}
+
+if (id == curhour){
+  var div = document.querySelector('div');
+  div.classList.replace('future', 'present');
+  div.classList.replace('past', 'present');
+}
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -8,13 +37,7 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
